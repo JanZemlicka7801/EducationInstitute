6a5682c714bf26f367c9bf3968488b92b061d52e
Jan Zemlicka <jan.zemlicka1@gmail.com>
2024-10-23 14:20:58 +0100

    Created a class to store a User.

---
+++ EducationInstitute/User.cs
@@ -0,0 +1,27 @@
+﻿namespace EducationInstitute;
+
+public class User
+{
+    private string UserId {get; set;}
+    private string Name {get; set;}
+    private string Email {get; set;}
+
+    public User(string userId, string name, string email)
+    {
+        UserId = userId;
+        Name = name;
+        Email = email;
+    }
+
+    public User()
+    {
+        UserId = "D00000000";
+        Name = "name";
+        Email = "D00000000@institute.com";
+    }
+    
+    public override string ToString()
+    {
+        return $"{nameof(UserId)}: {UserId}, {nameof(Name)}: {Name}, {nameof(Email)}: {Email}";
+    }
+}
