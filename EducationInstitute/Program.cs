3342f0e4459156c994139a6b590ee0b8ebb689d6
Jan Zemlicka <jan.zemlicka1@gmail.com>
2024-10-23 17:23:20 +0100

    Wrote a program that will ask a user to add new modules.

--- EducationInstitute/Program.cs
+++ EducationInstitute/Program.cs
@@ -1,3 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using EducationInstitute;
 
-Console.WriteLine("Hello, World!");
+static void CreateModules()
+{
+    Console.WriteLine("Create new module...");
+
+    Module module1 = CreateModuleFromInput();
+    Console.WriteLine("New module created: " + module1);
+}
+
+static Module CreateModuleFromInput()
+{
+    Console.Write("Enter the Module ID: ");
+    string moduleID = Console.ReadLine();
+
+    Console.Write("Enter the Module Name: ");
+    int moduleName = int.Parse(Console.ReadLine());
+
+    Console.Write("Enter the number of Credits: ");
+    int credits = int.Parse(Console.ReadLine());
+
+    return new Module(moduleID, moduleName, credits);
+}
+
+CreateModules();
