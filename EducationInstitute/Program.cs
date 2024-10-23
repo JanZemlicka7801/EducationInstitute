54dd7524a3833ddec65419076359fc95aef629fb
Jan Zemlicka <jan.zemlicka1@gmail.com>
2024-10-23 22:20:04 +0100

    Created a function for counting mature students in the class.

--- EducationInstitute/Program.cs
+++ EducationInstitute/Program.cs
@@ -24,10 +24,29 @@ static Module CreateModuleFromInput()
     return new Module(title, stage, credits);
 }
 
-Student testStudent = new Student("D00260376", "Jan", "D00260376@dkit.student.ie", "Computing", 3);
+static int CountMature(List<Student> students)
+{
+    int count = 0;
+    foreach (var student in students)
+    {
+        if (student.IsMature)
+        {
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+Student testStudent1 = new Student("D00260376", "Jan", "D00260376@dkit.student.ie", "Computing", 3, true);
+Student testStudent2 = new Student("D00260377", "Tom", "D00260377@dkit.student.ie", "Software Development", 2, true);
+Student testStudent3 = new Student("D00260378", "Lukas", "D00260378@dkit.student.ie", "Computing", 1, false);
+Student testStudent4 = new Student("D00260379", "Joe", "D00260379@dkit.student.ie", "Data Analytics", 4, true);
 Teacher testTeacher = new Teacher("D00260333", "Kinga", "D00260333@dkit.teacher.ie", 8, DateTime.Today);
+List<Student> students = new List<Student>() { testStudent1, testStudent2, testStudent3, testStudent4 };
 Module toAdd = CreateModules();
-testStudent.AddModule(toAdd);
+testStudent1.AddModule(toAdd);
 testTeacher.AddModule(toAdd);
-Console.WriteLine(testStudent);
+Console.WriteLine(testStudent1);
 Console.WriteLine(testTeacher);
+Console.WriteLine($"There are {CountMature(students)} student/s that is/are mature.");
